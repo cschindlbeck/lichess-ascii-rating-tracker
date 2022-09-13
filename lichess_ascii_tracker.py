f623fb3b90b4c4167eba22eb11901d1bf7caa8a3
Chris Schindlbeck <christopher.schindlbeck@iav.de>
2022-09-13 20:14:36 +0200

    Outsourced variables as env variables

--- lichess_ascii_tracker.py
+++ lichess_ascii_tracker.py
@@ -1,17 +1,22 @@
 #!/usr/local/bin/python3
+
+import os
 import berserk
 import asciichartpy
 
-API_TOKEN = "lip_oWEJVloOyWGv5eY16PTw"
-RATING_TYPE = 'Puzzles'
 
-def get_ratings_from_lichess() -> list:
+def get_ratings_from_lichess(API_TOKEN: str = None, RATING_TYPE: str = None) -> list:
     """
     Returns list of ratings from lichess using berserk
 
     :return: list of ratings from lichess
     :rtype: list
     """
+    if API_TOKEN is None: 
+        raise Exception("API_TOKEN must be passed")
+    if RATING_TYPE is None: 
+        raise Exception("RATING_TYPE must be passed")
+
     session = berserk.TokenSession(API_TOKEN)
     client = berserk.Client(session=session)
     user_id = client.account.get()['id']
@@ -22,7 +27,7 @@ def get_ratings_from_lichess() -> list:
     puzzle_type = RATING_TYPE
     puzzle_types = [d['name'] for d in rating_history if 'name' in d]
     if puzzle_type not in puzzle_types:
-        raise Exception(f"Puzzle type is not valid, you chose {puzzle_type}! Please use one of these: {puzzle_types}")
+        raise Exception(f"Puzzle type is not valid, you chose {puzzle_type}. Please use one of these: {puzzle_types}")
     print(f"{puzzle_type}")
 
     puzzle_rating_points = [d['points'] for d in rating_history if d['name'] == puzzle_type][0]
@@ -39,8 +44,26 @@ def print_ascii_tracker(ratings: list) -> None:
     config = {'height': 9, 'format': '{:8.0f}'}
     print(asciichartpy.plot(ratings, config))
 
+def get_token_from_env():
+    try:
+        API_TOKEN = os.environ['API_TOKEN']
+    except KeyError as keyerr:
+        raise Exception(f"Environment variable {keyerr} does not exist") from keyerr
+
+    return API_TOKEN
+
+def get_rating_type_from_env():
+    try:
+        RATING_TYPE = os.environ['RATING_TYPE']
+    except KeyError as keyerr:
+        raise Exception(f"Environment variable {keyerr} does not exist") from keyerr
+
+    return RATING_TYPE
+
 def main():
-    list_of_ratings = get_ratings_from_lichess()
+    API_TOKEN=get_token_from_env()
+    RATING_TYPE=get_rating_type_from_env()
+    list_of_ratings = get_ratings_from_lichess(API_TOKEN, RATING_TYPE)
     print_ascii_tracker(list_of_ratings)
 
 
